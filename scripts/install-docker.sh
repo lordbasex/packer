2b8070fa630964f85441aba5520aeadb1ae09251
Federico Pereira <lord.basex@gmail.com>
2021-12-17 14:41:08 -0300

    Update install-docker.sh

--- scripts/install-docker.sh
+++ scripts/install-docker.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
-sudo yum -y update
-sudo yum -y install curl git wget mc screen htop
-sudo curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
-sudo curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o  /usr/bin/docker-compose
-sudo chmod +x /usr/bin/docker-compose
+sudo su
+yum -y update
+yum -y install curl git wget mc screen htop
+
+yum -y install docker
+
+amazon-linux-extras install -y epel
+yum -y install p7zip
+
+curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compose
+chmod +x /usr/bin/docker-compose
+
+mkdir -p /etc/docker
+cat > /etc/docker/daemon.json <<ENDLINE
+{
+  "bip": "172.17.0.1/24",
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "10m"
+  }
+}
+ENDLINE
+
+
+systemctl enable docker
